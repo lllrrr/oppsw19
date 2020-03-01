5fda40966a8babce3e2c30e78cdda4be2c159fcc
lllrrr <6911742@qq.com>
2020-03-01 15:43:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -57,6 +57,7 @@ svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-qb
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/baidupcs-web package/lean/baidupcs-web
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-baidupcs-web package/lean/luci-app-baidupcs-web
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier package/lean/luci-app-zerotier
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-lib-fs package/lean/luci-lib-fs
 svn checkout https://github.com/Lienol/openwrt-package/trunk/lienol package/lienol/lienol
 svn checkout https://github.com/Lienol/openwrt-package/trunk/others package/lienol/others
 svn checkout https://github.com/Lienol/openwrt-package/trunk/package package/lienol/package
